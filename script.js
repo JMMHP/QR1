d6ef4d7e2cd40af8404bae7deb99cac47739bf63
JMMHP <149404100+JMMHP@users.noreply.github.com>
2023-10-30 13:19:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,44 @@
-// Function to generate a QR Code based on user input
 function generateQR() {
    const data = document.getElementById('qr-input').value;
    const qrOutput = document.getElementById('qr-output');
    const downloadLink = document.getElementById('download-link');
    if (data.trim() === '') return;
-   const typeNumber = 0;
-   const errorCorrectionLevel = 'L';
-   const qr = qrcode(typeNumber, errorCorrectionLevel);
-   qr.addData(data);
-   qr.make();
-   qrOutput.src = qr.createDataURL(6);
-   downloadLink.href = qrOutput.src;
-   qrOutput.style.display = 'block';
+   let qr = new QRCode(qrOutput, {
+       text: data,
+       width: 128,
+       height: 128
+   });
+   let img = qr._oDrawing._elImage; // Grab the image element from QRCode library
+   setTimeout(() => {
+       downloadLink.href = img.src; // Set the source for the download link after a short delay
+   }, 200);
    downloadLink.style.display = 'block';
 }
-// Function to read uploaded QR code and convert it to Excel
-function readQR() {
-   const fileInput = document.getElementById('qr-upload');
+function convertToExcel() {
+   const fileInput = document.getElementById('upload');
    const file = fileInput.files[0];
-   if (file) {
-       const reader = new FileReader();
-       reader.onload = function(e) {
-           const image = new Image();
-           image.onload = function() {
-               const canvas = document.createElement('canvas');
-               canvas.width = image.width;
-               canvas.height = image.height;
-               const ctx = canvas.getContext('2d');
-               ctx.drawImage(image, 0, 0);
-               const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-               const code = jsQR(imageData.data, imageData.width, imageData.height);
-               if (code) {
-                   const qrResult = document.getElementById('qr-result');
-                   qrResult.textContent = "QR Code Data: " + code.data;
-                   // Convert QR data to Excel
-                   const ws = XLSX.utils.json_to_sheet([{ 'QR Data': code.data }]);
-                   const wb = XLSX.utils.book_new();
-                   XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
-                   const excelData = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
-                   // Provide download link for the Excel file
-                   const blob = new Blob([new Uint8Array(excelData)], { type: 'application/octet-stream' });
-                   const link = document.createElement('a');
-                   link.href = window.URL.createObjectURL(blob);
-                   link.download = 'qrcode_data.xlsx';
-                   link.click();
-               }
-           };
-           image.src = e.target.result;
+   if (!file) return;
+   const reader = new FileReader();
+   reader.readAsDataURL(file);
+   reader.onload = function() {
+       // Decode the QR code
+       const image = new Image();
+       image.src = reader.result;
+       image.onload = function() {
+           const canvas = document.createElement('canvas');
+           const context = canvas.getContext('2d');
+           canvas.width = image.width;
+           canvas.height = image.height;
+           context.drawImage(image, 0, 0);
+           // Use QRCode library to decode
+           const decodedData = QRCode.decode(canvas.toDataURL('image/png'));
+           // Convert decoded data to Excel and download
+           // (for simplicity, assuming decodedData is comma-separated values)
+           const blob = new Blob([decodedData], { type: 'application/vnd.ms-excel' });
+           const url = URL.createObjectURL(blob);
+           const excelLink = document.getElementById('excel-download-link');
+           excelLink.href = url;
+           excelLink.style.display = 'block';
        };
-       reader.readAsDataURL(file);
    }
 }
