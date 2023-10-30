9efa51b65ac86ca91e3883a9a34a74bae6880dc0
JMMHP <149404100+JMMHP@users.noreply.github.com>
2023-10-30 14:13:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,62 @@
-// Generate QR code based on user input
-document.getElementById('generateQRBtn').addEventListener('click', async () => {
-   const content = document.getElementById('qrContent').value;
-   const canvas = document.getElementById('qrCanvas');
-   await QRCode.toCanvas(canvas, content);
-   const link = document.getElementById('downloadQRLink');
-   link.href = canvas.toDataURL();
-   link.download = 'generated_qr.png';
-   link.style.display = 'block';
-});
-// Upload and decode QR code
-document.getElementById('qrInput').addEventListener('change', async (event) => {
-   const file = event.target.files[0];
-   const reader = new FileReader();
-   reader.onload = async (e) => {
-       const decoded = await QRCode.toDataURL(e.target.result);
-       console.log('Decoded QR:', decoded);
-       // Note: You can store this decoded data or display it on the page.
-   };
-   reader.readAsDataURL(file);
-});
-// Upload Excel, append QR data, and download updated Excel
-document.getElementById('appendDataBtn').addEventListener('click', () => {
+function addField() {
+   let container = document.getElementById('inputFields');
+   let newField = document.createElement('div');
+   newField.className = "field-container";
+   newField.innerHTML = `
+<input type="text" placeholder="Field Name" class="field-name">
+<input type="text" placeholder="Field Value" class="field-value">
+   `;
+   container.appendChild(newField);
+}
+function generateQRCode() {
+   let fieldNames = document.querySelectorAll('.field-name');
+   let fieldValues = document.querySelectorAll('.field-value');
+   let data = [];
+   for (let i = 0; i < fieldNames.length; i++) {
+       data.push(`${fieldNames[i].value}: ${fieldValues[i].value}`);
+   }
+   let qrcode = new QRCode(document.getElementById("qrcode"), {
+       text: data.join('\n'),
+       width: 128,
+       height: 128,
+       colorDark: "#000000",
+       colorLight: "#ffffff",
+       correctLevel: QRCode.CorrectLevel.L
+   });
+   setTimeout(() => {
+       let downloadLink = document.getElementById('downloadLink');
+       downloadLink.href = document.querySelector('#qrcode img').src;
+       downloadLink.style.display = 'block';
+   }, 100);
+}
+async function appendDataToExcel() {
    const qrFile = document.getElementById('qrInput').files[0];
    const excelFile = document.getElementById('excelInput').files[0];
    const qrReader = new FileReader();
    const excelReader = new FileReader();
    let qrData;
    qrReader.onload = async (e) => {
-       qrData = await QRCode.toDataURL(e.target.result);
-       excelReader.readAsBinaryString(excelFile);
+       try {
+           qrData = await QRCode.decode(e.target.result);
+           excelReader.readAsBinaryString(excelFile);
+       } catch (error) {
+           console.error("Failed to decode QR:", error);
+       }
    };
    excelReader.onload = (e) => {
        const workbook = XLSX.read(e.target.result, { type: 'binary' });
-       // Assuming QR data is CSV and the first worksheet of Excel is the target
        const ws = workbook.Sheets[workbook.SheetNames[0]];
        const csvData = XLSX.utils.sheet_to_csv(ws);
        const combinedData = csvData + '\n' + qrData;
        const newWs = XLSX.utils.csv_to_sheet(combinedData);
-       // Update the workbook and write it back to Excel
        workbook.Sheets[workbook.SheetNames[0]] = newWs;
        const updatedExcel = XLSX.write(workbook, { bookType: 'xlsx', bookSST: true, type: 'binary' });
        const blob = new Blob([updatedExcel], { type: 'application/octet-stream' });
-       const link = document.getElementById('downloadLink');
+       const link = document.getElementById('downloadExcelLink');
        link.href = URL.createObjectURL(blob);
        link.download = 'updated_excel.xlsx';
        link.style.display = 'block';
    };
    qrReader.readAsDataURL(qrFile);
-});
+}
+// Place this code at the bottom of your HTML or in an external JavaScript file.
