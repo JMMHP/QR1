53c7f2a28c3ed55d1e4f121059ea08d0d10fc2d5
JMMHP <149404100+JMMHP@users.noreply.github.com>
2023-10-30 13:30:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,54 @@
-function generateQR() {
-   const data = document.getElementById('qr-input').value;
-   const qrOutput = document.getElementById('qr-output');
-   const downloadLink = document.getElementById('download-link');
-   if (data.trim() === '') return;
-   let qr = new QRCode(qrOutput, {
-       text: data,
-       width: 128,
-       height: 128
-   });
-   let img = qr._oDrawing._elImage; // Grab the image element from QRCode library
-   setTimeout(() => {
-       downloadLink.href = img.src; // Set the source for the download link after a short delay
-   }, 200);
-   downloadLink.style.display = 'block';
-}
-function convertToExcel() {
-   const fileInput = document.getElementById('upload');
-   const file = fileInput.files[0];
-   if (!file) return;
+// Import necessary libraries
+const QRCode = require('qrcode');
+const { decode } = require('qrcode/decoder');
+const XLSX = require('xlsx');
+// Generate QR code based on user input
+document.getElementById('generateQRBtn').addEventListener('click', async () => {
+   const content = document.getElementById('qrContent').value;
+   const canvas = document.getElementById('qrCanvas');
+   await QRCode.toCanvas(canvas, content);
+   const link = document.getElementById('downloadQRLink');
+   link.href = canvas.toDataURL();
+   link.download = 'generated_qr.png';
+   link.style.display = 'block';
+});
+// Upload and decode QR code
+document.getElementById('qrInput').addEventListener('change', async (event) => {
+   const file = event.target.files[0];
    const reader = new FileReader();
+   reader.onload = async (e) => {
+       const decoded = await decode(e.target.result);
+       console.log('Decoded QR:', decoded);
+       // Note: You can store this decoded data or display it on the page.
+   };
    reader.readAsDataURL(file);
-   reader.onload = function() {
-       // Decode the QR code
-       const image = new Image();
-       image.src = reader.result;
-       image.onload = function() {
-           const canvas = document.createElement('canvas');
-           const context = canvas.getContext('2d');
-           canvas.width = image.width;
-           canvas.height = image.height;
-           context.drawImage(image, 0, 0);
-           // Use QRCode library to decode
-           const decodedData = QRCode.decode(canvas.toDataURL('image/png'));
-           // Convert decoded data to Excel and download
-           // (for simplicity, assuming decodedData is comma-separated values)
-           const blob = new Blob([decodedData], { type: 'application/vnd.ms-excel' });
-           const url = URL.createObjectURL(blob);
-           const excelLink = document.getElementById('excel-download-link');
-           excelLink.href = url;
-           excelLink.style.display = 'block';
-       };
-   }
-}
+});
+// Upload Excel, append QR data, and download updated Excel
+document.getElementById('appendDataBtn').addEventListener('click', () => {
+   const qrFile = document.getElementById('qrInput').files[0];
+   const excelFile = document.getElementById('excelInput').files[0];
+   const qrReader = new FileReader();
+   const excelReader = new FileReader();
+   let qrData;
+   qrReader.onload = async (e) => {
+       qrData = await decode(e.target.result);
+       excelReader.readAsBinaryString(excelFile);
+   };
+   excelReader.onload = (e) => {
+       const workbook = XLSX.read(e.target.result, { type: 'binary' });
+       // Assuming QR data is CSV and the first worksheet of Excel is the target
+       const ws = workbook.Sheets[workbook.SheetNames[0]];
+       const csvData = XLSX.utils.sheet_to_csv(ws);
+       const combinedData = csvData + '\n' + qrData;
+       const newWs = XLSX.utils.csv_to_sheet(combinedData);
+       // Update the workbook and write it back to Excel
+       workbook.Sheets[workbook.SheetNames[0]] = newWs;
+       const updatedExcel = XLSX.write(workbook, { bookType: 'xlsx', bookSST: true, type: 'binary' });
+       const blob = new Blob([updatedExcel], { type: 'application/octet-stream' });
+       const link = document.getElementById('downloadLink');
+       link.href = URL.createObjectURL(blob);
+       link.download = 'updated_excel.xlsx';
+       link.style.display = 'block';
+   };
+   qrReader.readAsDataURL(qrFile);
+});
